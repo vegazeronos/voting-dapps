f0397348b2d79992869bf0f99d151c6fbf2c98ca
Ravfael <ravfaelnovitohandoyo419@gmail.com>
2025-04-11 11:32:08 +0700

    custom dikit nav sama Text

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,52 +1,30 @@
-
 import Link from "next/link";
 import Image from "next/image";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
-
-
-
 const Navigation = () => {
   return (
-    <nav className="bg-[var(--background)] border-b-4 border-[var(--foreground)]/10">
+    <nav className="bg-[var(--background)] border-b border-[var(--navbar-border)]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16 items-center">
+        <div className="flex justify-between h-19 items-center">
           {/* Logo dan Navigation Links di Samping */}
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-7">
             {/* Logo */}
             <div className="flex-shrink-0">
-              <Image
-                src="/logo.png"
-                alt="TrustVote Logo"
-                width={60}
-                height={60}
-                className="rounded-full"
-              />
+              <Image src="/logo.png" alt="TrustVote Logo" width={60} height={60} className="rounded-full" />
             </div>
 
             {/* Navigation Links */}
-            <Link
-              href="/#welcome"
-              className="text-gray-400 hover:text-white transition-colors"
-              scroll={true}
-            >
+            <Link href="/#welcome" className="text-gray-400 hover:text-white transition-colors" scroll={true}>
               How It Works
             </Link>
-            <Link
-              href="/top-vote"
-              className="text-gray-400 hover:text-white transition-colors"
-            >
+            <Link href="/top-vote" className="text-gray-400 hover:text-white transition-colors">
               Top Vote
             </Link>
           </div>
 
           {/* Connect to Wallet Text */}
-          <ConnectButton 
-            label="Connect to Wallet"
-            showBalance={false}
-            chainStatus={"none"}
-            accountStatus={"address"}
-          />
+          <ConnectButton label="Connect to Wallet" showBalance={false} chainStatus={"none"} accountStatus={"address"} />
         </div>
       </div>
     </nav>
